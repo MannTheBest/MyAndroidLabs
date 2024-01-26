8caa797a616ef804a66136656ecf67c5399a32f3
Man Patel <mannpatel520@gmail.com>
2024-01-25 19:56:35 -0500

    Initial Commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Mann's Android Labs"
+include(":app")
+ 
