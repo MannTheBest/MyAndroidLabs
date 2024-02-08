bca56a4a7039418829a89250347e22380049e28e
Man Patel <mannpatel520@gmail.com>
2024-02-07 20:43:49 -0500

    Finished Phone Version of Lab 3

--- app/src/main/java/algonquin/cst2335/mann0520/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/mann0520/MainActivity.java
@@ -3,12 +3,36 @@ package algonquin.cst2335.mann0520;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView img1;
+    Switch switch1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        switch1 = findViewById(R.id.spin_switch);
+        img1 = findViewById(R.id.flagview);
+
+        switch1.setOnCheckedChangeListener( (btn, isChecked) -> {
+            if (isChecked)
+            {
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f,
+                        Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
+                img1.startAnimation(rotate);
+            }else {
+                img1.clearAnimation();
+            }
+        });
     }
 }
