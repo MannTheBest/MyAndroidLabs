ddabc53ef9b2cb5d1b98480b527ef7bd156e33c2
Man Patel <mannpatel520@gmail.com>
2024-02-15 20:20:30 -0500

    Finished basic string RecycleView

---
+++ app/src/main/java/algonquin/cst2335/mann0520/data/ChatRoomViewModel.java
@@ -0,0 +1,11 @@
+package algonquin.cst2335.mann0520.data;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.ArrayList;
+
+public class ChatRoomViewModel extends ViewModel {
+
+    public MutableLiveData<ArrayList<String>> messages = new MutableLiveData<>();
+}
